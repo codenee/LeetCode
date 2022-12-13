d1934137296c3dc2167796b6cb0ed14925049154
codenee <119647209+codenee@users.noreply.github.com>
2022-12-13 17:27:06 +0900

    Time: 36 ms (73.04%), Space: 14.3 MB (12.82%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -18,20 +18,9 @@ public:
 };
 */
 
-/*
-       if (!root) return {};
-        vector<int> v;
-        v.push_back(root->val);
-        for (int i=0; i<root->children.size(); i++) {
-            vector<int>v1 = preorder(root->children[i]);
-            v.insert(v.end(), v1.begin(), v1.end());
-        }
-        return v;
-*/
-
 class Solution {
 public:
-    vector<int> preorder(Node* root) {
+    vector<int> preorder2(Node* root) {
         if(!root){
             return {};
         }
@@ -47,6 +36,21 @@ public:
                 s.push(node->children[i]);
             }
         }
+        return res;
+    }
+
+    vector<int> preorder(Node* root) {
+        if(!root){
+            return {};
+        }
+        
+        std::vector<int> res;
+        res.push_back(root->val);
+        for(int i = 0; i < root->children.size(); i++){
+            std::vector<int> v1 = preorder(root->children[i]);
+            res.insert(res.end(), v1.begin(), v1.end());
+        }
+
         return res;
     }
 };
