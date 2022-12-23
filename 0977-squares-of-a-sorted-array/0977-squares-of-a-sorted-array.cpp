f0e9617b36885d8428f095acf76186d1fd45e006
codenee <119647209+codenee@users.noreply.github.com>
2022-12-23 14:15:03 +0900

    Time: 82 ms (40.16%), Space: 25.9 MB (56.10%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
+    //solution 1
+    vector<int> sortedSquares2(vector<int>& nums) {
         vector<int> result;
         for(const auto& it: nums){
             result.push_back(it * it);
         }
         sort(result.begin(), result.end());
+        return result;
+    }
+    //solution 2
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int left = 0, right = nums.size() - 1;
+        for(int k = nums.size() - 1; k >= 0; k--){
+            if(abs(nums[right]) > abs(nums[left])){
+                result[k] = nums[right] * nums[right--]; 
+            }
+            else{
+                result[k] = nums[left] * nums[left++];
+            }
+        }
+        
         return result;
     }
 };
