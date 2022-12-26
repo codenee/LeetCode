cccb3a39ff0cd69734a880c5a35bfeaa26ceb9c2
codenee <119647209+codenee@users.noreply.github.com>
2022-12-26 17:58:52 +0900

    Time: 15 ms (84.27%), Space: 9.8 MB (49.46%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,35 +1,16 @@
 class Solution {
-private:
-    void parseString(string& str, vector<string>& vec, const string& delimiter){
-        
-        int pos = 0;
-        while( (pos = str.find(delimiter)) != string::npos ){
-            
-            string tmp = str.substr(0, pos);
-            vec.push_back(tmp);
-            str.erase(0, pos + delimiter.length());
-        }
-        vec.push_back(str);
-        
-    }
 public:
     string reverseWords(string s) {
         
-        vector<string> vec;
-        string delimiter(" ");
-        parseString(s, vec, delimiter);
-        
-        string result("");
-        for(const auto& it : vec){
+        int i = 0;
+        for(int j = 0; j < s.size(); ++j){
             
-            string tmp = it;
-            int i = 0, j = it.size() -1;
-            while( i < j){
-                swap(tmp[i++], tmp[j--]);    
+            if( s[j] == ' '){
+                reverse(s.begin() + i, s.begin() + j);
+                i = j + 1;
             }
-            result += tmp + " ";
         }
-        result.erase(result.size()-1);
-        return result;
+        reverse(s.begin() + i, s.end());
+        return s;
     }
 };
