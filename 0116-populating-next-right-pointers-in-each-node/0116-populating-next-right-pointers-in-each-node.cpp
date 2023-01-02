46fc872b19b7ebcabd07f711d61bd490c4c96cb9
codenee <119647209+codenee@users.noreply.github.com>
2023-01-02 21:52:53 +0900

    Time: 15 ms (97.75%), Space: 16.8 MB (69.18%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -20,18 +20,18 @@ class Solution {
 public:
     Node* connect(Node* root) {
         
-        if(!root){
-            return NULL;
+        Node *prev = root, * curr;
+        while(prev){
+            curr = prev;
+            while(curr && curr->left){
+                curr->left->next = curr->right;
+                if(curr->next){
+                    curr->right->next = curr->next->left;
+                }
+                curr = curr->next;
+            }
+            prev = prev->left;
         }
-        if(root->left){
-            root->left->next = root->right;
-        }
-        if(root->right && root->next){
-            root->right->next = root->next->left;
-        }
-        connect(root->left);
-        connect(root->right);
-        
         return root;
     }
 };
